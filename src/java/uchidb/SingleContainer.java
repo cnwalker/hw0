79816a34f17c871964a3f2a7f9ce672f9a8968b2
Christopher Walker <cnwalker@uchicago.edu>
2017-01-08 18:49:41 -0600

    Implemented set, list, and map methods

--- src/java/uchidb/SingleContainer.java
+++ src/java/uchidb/SingleContainer.java
@@ -8,13 +8,21 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class SingleContainer<T, S> implements Containers<T, S> {
+    private Map<S, T> innerMap;
+
     public Set<T> initSet(T[] tArray) {
         Set<T> set = new HashSet<T>();
+        for (int i = 0; i < tArray.length; i++) {
+            set.add(tArray[i]);
+        }
         return set;
     }
 
     public List<T> initList(T[] tArray) {
         List<T> list = new ArrayList<T>();
+        for (int i = 0; i < tArray.length; i++) {
+            list.add(tArray[i]);
+        }
         return list;
     }
 
@@ -24,19 +32,26 @@ public class SingleContainer<T, S> implements Containers<T, S> {
     }
 
     public void storeMap(Map<S, T> mapToStoreInClass) {
-        return;
+        innerMap = mapToStoreInClass;
     }
 
     public boolean addToMap(S key, T value, boolean overwriteExistingKey) {
-        return false;
+        if (innerMap.containsKey(key) && !overwriteExistingKey) {
+            return false;
+        }
+
+        innerMap.put(key, value);
+        return true;
     }
 
     public T getValueFromMap(S key) {
-        T value = null;
-        return value;
+        return innerMap.get(key);
     }
 
     public T getValueFromMap(S key, T defaultValue) {
+        if (innerMap.get(key) != null) {
+            return innerMap.get(key);
+        }
         return defaultValue;
     }
 }
